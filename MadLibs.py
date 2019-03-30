976820f3853a7c8f3dd962b46e54fb8ba8e61ef0
Dokgox <47923843+Dokgox@users.noreply.github.com>
2019-03-30 10:45:01 -0500

    Update MadLibs.py
    # added

--- MadLibs.py
+++ MadLibs.py
@@ -1,3 +1,5 @@
+
+#user enters words
 noun 1 = input(“Enter a noun”)
 place = input(“Enter a place”)
 action =  input(“Enter an action”)
@@ -8,6 +10,7 @@ adjective 1 = input(“Enter an adjective”)
 adjective 2 = input(“Enter an adjective”)
 emotion = input(“Enter an emotion”)
 
+#entered words are put into the story 
 print(“Once upon a time there lived a ” + noun 1)
 print(noun 1 + “ needed to ” + action “ at ” + place)
 print(noun 1 + “ started on ” + pronoun “  journey”)
